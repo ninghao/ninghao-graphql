6c27b48f85bc1647c9abff96f1ab6e71c61708dd
wanghao8080 <117663444@qq.com>
2017-12-27 15:22:47 +0800

    用 Express 创建 GraphQL HTTP 服务

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
 
 const app = express()
 
-app.listen(8080)
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+app.listen(3000)
